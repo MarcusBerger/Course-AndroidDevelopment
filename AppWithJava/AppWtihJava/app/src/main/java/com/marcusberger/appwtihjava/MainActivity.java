6c53c2972ad14e0721ccb42713da15479ab0a504
MarcusBerger <marcusc-berger@hotmail.com>
2015-09-12 22:07:14 -0300

    AddVariable

--- AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
+++ AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 import com.marcusberger.appwtihjava.R;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -17,7 +19,10 @@ public class MainActivity extends ActionBarActivity {
 
     public void submitOrder(View view) {
 
-        display(77*2+1);
+        int quantity = 5;
+
+        display(quantity);
+        displayPrice(quantity*5);
 
     }
 
@@ -25,5 +30,10 @@ public class MainActivity extends ActionBarActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
