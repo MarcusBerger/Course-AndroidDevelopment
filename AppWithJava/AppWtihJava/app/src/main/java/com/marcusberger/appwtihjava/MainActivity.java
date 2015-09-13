f32ed17c5b42351b1e997690a2dc4d53ea364e05
MarcusBerger <marcusc-berger@hotmail.com>
2015-09-13 02:33:29 -0300

    AddCoffeQuantity

--- AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
+++ AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.marcusberger.appwtihjava;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,14 +18,35 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOrder(View view) {
+    int quantity = 0;
 
-        int quantity = 5;
+    public void increment(View view) {
 
+        quantity = quantity+1;
         display(quantity);
-        displayPrice(quantity*5);
 
     }
+
+    public void decrement(View view) {
+
+        quantity = quantity-1;
+        display(quantity);
+
+    }
+
+    public void submitOrder(View view) {
+
+        int price = quantity * 5;
+        String priceMessage = "Price $: " + price;
+        priceMessage = priceMessage + "\nthank you!!";
+        displayMessage(priceMessage);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
@@ -36,4 +58,5 @@ public class MainActivity extends ActionBarActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
 }
