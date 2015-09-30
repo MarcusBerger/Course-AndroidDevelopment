cd92fd5424a13cae6d099f0130961b5e424079e6
MarcusBerger <marcusc-berger@hotmail.com>
2015-09-30 03:43:52 -0300

    Bagunçou, arrumo depois

--- AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
+++ AppWithJava/AppWtihJava/app/src/main/java/com/marcusberger/appwtihjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Display;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.marcusberger.appwtihjava.R;
@@ -18,45 +19,18 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
-
-    public void increment(View view) {
-
-        quantity = quantity+1;
-        display(quantity);
-
-    }
-
-    public void decrement(View view) {
-
-        quantity = quantity-1;
-        display(quantity);
-
-    }
-
     public void submitOrder(View view) {
 
-        int price = quantity * 5;
-        String priceMessage = "Price $: " + price;
-        priceMessage = priceMessage + "\nthank you!!";
-        displayMessage(priceMessage);
+        String gerarMessage = "Frase";
+        gerarMessage = gerarMessage;
+        displayMessage(gerarMessage);
+        TextView Frase = (TextView) findViewById(
+                R.id.Frase);
     }
 
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
-
-
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        TextView gerarFrase = (TextView) findViewById(R.id.Frase);
+        gerarFrase.setText(message);
     }
 
 }
